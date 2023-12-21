e8e040becf8f393d0cca42442ea7f365b0636d11
NimeshaHewawasam <nimesha93swj@gmail.com>
2023-12-21 22:23:46 +0530

    Create Check-Service-Status Script File

---
+++ check-service-status.py
@@ -0,0 +1,46 @@
+from random import random
+"""
+1. Get httpd, rabbitMQ, postgreSQL Status 
+2. Save to {serviceName}-status-{@timestamp}.json
+3. Payload to POST:http://<< service >>/add 
+
+"""
+def is_up_or_down():
+    # This method for get service status either UP Or DOWN as Radom
+    random_number = random()
+    if random_number > 0.5: 
+        return "UP"
+    else:
+        return "DOWN"
+
+def get_httpd_status() :
+    return {
+        "service_name":"httpd",
+        "service_status": is_up_or_down(),
+        "host_name":"host1"
+    }
+
+def get_rabbitMQ_status() :
+    return {
+        "service_name":"rabbitMQ",
+        "service_status": is_up_or_down(),
+        "host_name":"host2"
+    }
+
+def get_postgreSQL_status() :
+    return {
+        "service_name":"postgreSQL",
+        "service_status": is_up_or_down(),
+        "host_name":"host3"
+    }
+
+def write_json_payload(service_name, paylaod) :
+    timestamp = '2023-12-21' # TODO : Genere Date Time
+    file_name = service_name + '-status-' + timestamp + '.json'
+    # TODO : Write a Json File
+
+write_json_payload('httpd', get_httpd_status())
+write_json_payload('rabbitMQ', get_rabbitMQ_status())
+write_json_payload('postgreSQL', get_postgreSQL_status())
+
+# TODO : Step 3 Here
